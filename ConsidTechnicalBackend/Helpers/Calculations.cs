c31797ffbb71d18ef218619f38dd30e5d7228275
Oskar Virdarson <80251567+oskarvird@users.noreply.github.com>
2023-03-08 09:02:39 +0100

    feat(Helpers)

---
+++ ConsidTechnicalBackend/Helpers/Calculations.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsidTechnicalBackend.Helpers;
+
+public class Calculations : ICalculations
+{
+    public decimal CalculateSalary(bool isCeo, bool isManager, int rank)
+    {
+        decimal salaryCoefficient = 1.125m;
+
+        if (isCeo)
+        {
+            salaryCoefficient = 2.725m;
+        }
+        else if (isManager)
+        {
+            salaryCoefficient = 1.725m;
+        }
+
+        decimal salary = rank * salaryCoefficient;
+
+        return salary;
+    }
+
+}
---
+++ ConsidTechnicalBackend/Helpers/ICalculations.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsidTechnicalBackend.Helpers;
+
+public interface ICalculations
+{
+    decimal CalculateSalary(bool isCeo, bool isManager, int rank);
+}
