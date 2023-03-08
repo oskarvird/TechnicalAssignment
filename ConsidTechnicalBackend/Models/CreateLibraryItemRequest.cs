f548f4891e3dbb84e8fe3a403fbb9a9da8ea67c5
Oskar Virdarson <80251567+oskarvird@users.noreply.github.com>
2023-03-08 08:57:57 +0100

    feat(Models)

--- ConsidTechnicalBackend/Models/CreateLibraryItemRequest.cs
+++ ConsidTechnicalBackend/Models/CreateLibraryItemRequest.cs
@@ -2,16 +2,15 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace ConsidTechnicalBackend.Models
+namespace ConsidTechnicalBackend.Models;
+
+public class CreateLibraryItemRequest
 {
-    public class CreateLibraryItemRequest
-    {
-        public int CategoryId { get; set; }
-        public DbCategory Category { get; set; } //TODO: kommer vi ta in id för kategori eller namnet?
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public bool IsBorrowable { get; set; }
-        public string Borrower { get; set; }
-        public string Type { get; set; } 
-    }
+    public int CategoryId { get; set; }
+    public DbCategory Category { get; set; } //TODO: kommer vi ta in id för kategori eller namnet?
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public bool IsBorrowable { get; set; }
+    public string Borrower { get; set; }
+    public string Type { get; set; } 
 }
---
+++ ConsidTechnicalBackend/Models/EmployeeGetResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsidTechnicalBackend.Models;
+
+public class EmployeeGetResponse
+{
+    public Employee Ceo { get; set; }
+    public List<Employee> Managers { get; set; }
+    public List<Employee> Employees { get; set; }
+}
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public decimal Salary { get; set; }
+}
